e364bbf6a50d9337d85587d5aa3aaf5db3c9c231
navod <navodthishan@gmail.com>
2025-04-25 09:02:58 +0530

    Trusted add

--- resources/frontend/src/pages/MainWeb/Home.tsx
+++ resources/frontend/src/pages/MainWeb/Home.tsx
@@ -3,6 +3,7 @@ import HeroSection from './Component/HeroSection';
 import Footer from './Footer/Footer';
 import Profile from './Component/Profile';
 import Benefits from './Component/Benefits';
+import Trusted from './Component/Trusted';
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
                 <HeroSection />
                 <Profile />
                 <Benefits />
+                <Trusted />
             </main>
             <Footer />
         </div>
